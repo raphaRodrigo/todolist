c12ba81a2be35c47c4a87ce5c0580d5ea15a5343
raphaRodrigo <rapha.7@hotmail.com>
2023-10-13 14:39:03 -0300

    first commit

---
+++ src/test/java/br/com/rapharodrigo/todolist/TodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rapharodrigo.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
