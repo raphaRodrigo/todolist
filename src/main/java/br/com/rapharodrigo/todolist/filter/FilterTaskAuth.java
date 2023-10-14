532bf14e83262c64fd8e0f94047a7cafe960bf68
raphaRodrigo <rapha.7@hotmail.com>
2023-10-14 14:42:35 -0300

    Added some date validations and a GET for requesting all tasks from a user

--- src/main/java/br/com/rapharodrigo/todolist/filter/FilterTaskAuth.java
+++ src/main/java/br/com/rapharodrigo/todolist/filter/FilterTaskAuth.java
@@ -4,12 +4,12 @@ import java.io.IOException;
 import java.util.Base64;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import at.favre.lib.crypto.bcrypt.BCrypt;
 import br.com.rapharodrigo.todolist.user.IUserRepository;
+import br.com.rapharodrigo.todolist.user.UserModel;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -25,13 +25,17 @@ public class FilterTaskAuth extends OncePerRequestFilter {
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
 
+        var servletPath = request.getServletPath();
+
+        if (!servletPath.equals("/tasks/")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
         var authorization = request.getHeader("Authorization");
-
         var encodedAuth = authorization.substring("Basic".length()).trim();
-
         var decodedAuth = Base64.getDecoder().decode(encodedAuth);
-
-        var authString = new String(decodedAuth);
+        var authString = new String(decodedAuth);        
         String[] credentials = authString.split(":");
         String username = credentials[0];
         String password = credentials[1];
@@ -40,15 +44,20 @@ public class FilterTaskAuth extends OncePerRequestFilter {
         if (user == null) {
             response.sendError(401);
         } else {
-            var passwordVerify = BCrypt.verifyer().verify(password.toCharArray(), user.getPassword());
-
-            if (passwordVerify.verified) {
-                filterChain.doFilter(request, response);
-            } else {
-                response.sendError(401);
-            }
+            verifyPassword(request, response, filterChain, password, user);
         }
+    }
 
+    private void verifyPassword(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain,
+            String password, UserModel user) throws IOException, ServletException {
+        var verifiedPassword = BCrypt.verifyer().verify(password.toCharArray(), user.getPassword());
+
+        if (verifiedPassword.verified) {
+            request.setAttribute("userId", user.getId());
+            filterChain.doFilter(request, response);
+        } else {
+            response.sendError(401);
+        }
     }
 
 }
--- src/main/java/br/com/rapharodrigo/todolist/task/TaskController.java
+++ src/main/java/br/com/rapharodrigo/todolist/task/TaskController.java
@@ -1,10 +1,19 @@
 package br.com.rapharodrigo.todolist.task;
 
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.servlet.http.HttpServletRequest;
 
 @RestController
 @RequestMapping("/tasks")
@@ -14,8 +23,30 @@ public class TaskController {
     private ITaskRepository taskRepository;
 
     @PostMapping("/")
-    public TaskModel create(@RequestBody TaskModel taskModel) {
+    public ResponseEntity create(@RequestBody TaskModel taskModel, HttpServletRequest request) {
+        var userId = request.getAttribute("userId");
+        taskModel.setUserId((UUID) userId);
+
+        var currentDate = LocalDateTime.now();
+        if (currentDate.isAfter(taskModel.getStartAt()) ||
+                currentDate.isAfter(taskModel.getEndAt())) {
+
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                    .body("Starting and ending date must be more recent than the current one.");
+        }
+        if (taskModel.getStartAt().isAfter(taskModel.getEndAt())) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                    .body("Starting date must be older than the ending one.");
+        }
+
         var task = this.taskRepository.save(taskModel);
-        return task;
+        return ResponseEntity.status(HttpStatus.OK).body(task);
+    }
+
+    @GetMapping("/")
+    public List<TaskModel> list(HttpServletRequest request) {
+        var userId = request.getAttribute("userId");
+        var tasks = this.taskRepository.findByUserId((UUID) userId);
+        return tasks;
     }
 }
