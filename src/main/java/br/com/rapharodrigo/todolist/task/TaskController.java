001d167a57818efb9a226de8e0e19a326dabbb69
raphaRodrigo <rapha.7@hotmail.com>
2023-10-15 11:13:20 -0300

    User is validated before altering a sent task

--- src/main/java/br/com/rapharodrigo/todolist/task/TaskController.java
+++ src/main/java/br/com/rapharodrigo/todolist/task/TaskController.java
@@ -54,9 +54,23 @@ public class TaskController {
     }
 
     @PutMapping("/{id}")
-    public TaskModel update(@RequestBody TaskModel taskModel, HttpServletRequest request, @PathVariable UUID id) {
+    public ResponseEntity update(@RequestBody TaskModel taskModel, HttpServletRequest request, @PathVariable UUID id) {
         var task = this.taskRepository.findById(id).orElse(null);
+
+        if (task == null) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                    .body("Task not found!");
+        }
+
+        var userId = request.getAttribute("userId");
+
+        if (!task.getUserId().equals(userId)){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                    .body("You don't have permission to alter this task!");
+        }
+
         Utils.copyNonNullProperties(taskModel, task);
-        return this.taskRepository.save(task);
+        var updatedTask = this.taskRepository.save(task);
+        return ResponseEntity.ok().body(updatedTask);
     }
 }
